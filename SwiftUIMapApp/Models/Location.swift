18b1c7082db4c9174b64c20d08773aae70fcdadf
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-12-21 18:45:44 +0200

    Project Setup with Assets and Custom Data Model

---
+++ SwiftUIMapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  SwiftUIMapApp
+//
+//  Created by ramil on 21.12.2021.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
